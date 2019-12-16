1cc8072516ab2138fdc7e0c36035cad09e6c5efa
madhu <madsivas@gmail.com>
2019-12-16 22:52:52 +0100

    Day 1 code cleanup

--- advent-of-code-2019/day1.py
+++ advent-of-code-2019/day1.py
@@ -10,7 +10,7 @@ def calculate_fuel_required(mass):
 def read_module_masses(filename):
     with open(filename) as f:
         mm_list = f.readlines()
-    # you may also want to remove whitespace characters like `\n` at the end of each line
+    # remove whitespace characters like '\n' at the end of each line
     mm_list = [x.strip() for x in mm_list]
     return mm_list
 
@@ -41,20 +41,15 @@ if __name__ == "__main__":
 
     fuel_for_modules = 0
     fuel_for_fuel = 0
-    fuel_for_module_incl_fuel_mass = list()
     for m in module_masses:
         fm = calculate_fuel_required(mass=float(m))
-        ff = calculate_fuel_required_for_fuel(fm)
+        ff = calculate_fuel_required_for_fuel(fm)  # part2 of day1 puzzle
         print("Fuel required for fuel of mass %s is %s" % (fm, ff))
         fuel_for_modules += fm
-        fuel_for_module_incl_fuel_mass.append(fm + ff)
         fuel_for_fuel += ff
 
-    print("Total fuel required for modules excl. fuel mass: %s" % fuel_for_modules)
-    print("Total fuel required for modules incl. fuel mass: %s" % sum(fuel_for_module_incl_fuel_mass))
-
-#    fuel_for_fuel = calculate_fuel_required_for_fuel(fuel_for_modules)
-    print("Total fuel required for fuel mass: %s" % fuel_for_fuel)
+    print("Fuel required for modules excl. fuel mass: %s" % fuel_for_modules)
+    print("Fuel required for fuel mass: %s" % fuel_for_fuel)
 
     fuel_total = fuel_for_modules + fuel_for_fuel
     print("Total fuel required = fuel for modules + fuel for fuel = %s" % fuel_total)
