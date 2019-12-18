8715343327b0225a0b655bcfed1b35a4a2763a0e
madsivas <madsivas@gmail.com>
2019-12-18 11:23:18 +0100

    precondition value resets + for loop changed to while
    added precondition value resets (from puzzle definition); for loop changed to while loop with index counter.

--- advent-of-code-2019/day2-intcode.py
+++ advent-of-code-2019/day2-intcode.py
@@ -36,27 +36,35 @@ def execute(instruction, int_code_seq):
     idx = output_index
     if operation == op_code_add:
         int_code_seq[output_index] = do_add(int_code_seq[operand_1_index], int_code_seq[operand_2_index])
-        print("Output index: %s, output value: %s" % (idx, int_code_seq[output_index]))
     elif operation == op_code_multiply:
         int_code_seq[output_index] = do_multiply(int_code_seq[operand_1_index], int_code_seq[operand_2_index])
-        print("Output index: %s, output value: %s" % (idx, int_code_seq[output_index]))
     elif operation == op_code_exit:
         return False
     else:
         print("Invalid or unknown operation %s! Terminating and going up in flames!" % operation)
         sys.exit(2)
 
+    print("Output index: %s, output value: %s" % (idx, int_code_seq[output_index]))
     return True
 
 
 def int_code_computer(int_code_seq):
     result = None
-    instructions = [int_code_seq[x:x+4] for x in range(0, len(int_code_seq), 4)]
-    print("# instructions to execute: %s" % len(instructions))
-    for instr in instructions:
+    #  instructions = [int_code_seq[x:x+4] for x in range(0, len(int_code_seq), 4)]
+    num_int_codes = len(int_code_seq)
+    num_instructions = int(num_int_codes / 4)
+    print("# instructions to execute: %s" % num_instructions)
+    idx = 0
+    while idx < num_int_codes:
+        instr = int_code_seq[idx:idx+4]
+        if idx < num_int_codes - 4:
+            idx += 4
+        else:
+            idx += num_int_codes - idx  # the last few int codes - may not be a multiple of 4
+
         indicator = execute(instr, int_code_seq)  # both args to execute function change each iteration
         if indicator is False:
-            break;
+            break
     return result
 
 
@@ -70,7 +78,11 @@ if __name__ == "__main__":
     param_file = "advent-of-code-d2-input.txt"
     int_codes = read_int_codes(param_file)
 
+    print("Int codes length: %s" % len(int_codes))
     print("Int codes before computing: %s" % int_codes)
+
+    int_codes[1] = 12  # pre_condition_value_reset_at_idx_1 = 12
+    int_codes[2] = 2  # pre_condition_value_reset_at_idx_2 = 2
     int_code_computer(int_codes)
 
     print("Int codes after computing: %s" % int_codes)
